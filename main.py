1babdd725026494e6dc5f1c8fd789ceb27531847
oaftobar <oaftobar@users.noreply.github.com>
2026-01-24 14:57:58 -0500

    B: the player can now move and turn

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        player.update(dt)
+
+        dt = pygame.time.Clock().tick(60) / 1000.0
+        # print(f"Delta time: {dt} seconds")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
